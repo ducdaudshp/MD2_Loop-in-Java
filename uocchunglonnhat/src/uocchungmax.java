2d6dcc51f8f646d4ee5e6acb9fed984d0a4012ca
ducdaudshp <ducdaudshp@gmail.com>
2022-02-08 11:24:20 +0700

    Uoc chung lon nhat

---
+++ uocchunglonnhat/src/uocchungmax.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class uocchungmax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập a= ");
+        int a = scanner.nextInt();
+        System.out.println("Nhập b= ");
+        int b = scanner.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            System.out.println("Không có ước chung lớn nhất");
+        }
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        System.out.println("Ước chung lớn nhất là : " + a);
+    }
+}
